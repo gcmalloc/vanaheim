3749f32d7837bca7c13c9dd869928168e64bdf08
Jean-Baptiste Carré <jbc@0x1d.org>
2012-07-01 22:19:52 +0200

    settings updates

--- settings.py
+++ settings.py
@@ -3,10 +3,11 @@
 
 SITE_TITLE = 'speredenn...'
 SITE_SUBTITLE = 'test'
-SITE_PATH = '/home/carre/src/vanaheim'
-DL_PATH = SITE_PATH + '/downloads'
-STATIC_PATH = SITE_PATH + '/static'
-SLIDES_PATH = SITE_PATH + '/slides'
-MOVIES_PATH = SITE_PATH + '/movies'
-PICS_PATH =  SITE_PATH + '/pics'
+# SITE_PATH = '/home/carre/src/vanaheim/'
+SITE_PATH = ''
+MUSIC_PATH = SITE_PATH + 'music'
+PICTURES_PATH = SITE_PATH + 'pics'
+STATIC_PATH = SITE_PATH + 'static'
+SLIDES_PATH = SITE_PATH + 'slides'
+MOVIES_PATH = SITE_PATH + 'movies'
 POSTS_PER_PAGE = 10
